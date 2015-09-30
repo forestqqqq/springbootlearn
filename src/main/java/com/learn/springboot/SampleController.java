8a8cfbf0b1fff33857d1da42267618a3d3aff2da
ZhangHaiwei <forestqqqq@163.com>
2015-09-30 13:52:55 +0800

    第一个版本程序

---
+++ src/main/java/com/learn/springboot/SampleController.java
@@ -0,0 +1,29 @@
+package com.learn.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author 作者： zhanghaiwei
+ * @version 1.0
+ * @date 创建时间：2015/9/30 10:27
+ * @since jdk1.6.0_27
+ */
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
